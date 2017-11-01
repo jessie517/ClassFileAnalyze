dc96248107d0221fe2d2cbbcef9c2bafc41b50cf
jessie517 <jessie_517@126.com>
2017-11-01 20:14:20 +0800

    StackMapTable解析

--- src/com/analyze/attribute/bean/StackMapTableAttributeBean.java
+++ src/com/analyze/attribute/bean/StackMapTableAttributeBean.java
@@ -1,12 +1,207 @@
 package com.analyze.attribute.bean;
 
+import com.analyze.attribute.AnnotationsAnalyze;
+import com.utils.UToNumeric;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
 /**
  * Created by chenjiaxu on 2017/10/31.
  */
 public class StackMapTableAttributeBean implements AttributeInfoBean {
+    private StackMapFrame[] entries;
 
-    public StackMapTableAttributeBean(byte[] infobytes){
-
+    public StackMapTableAttributeBean(byte[] infoBytes) throws Exception {
+        int i = 0;
+        int numberOfEntries = (infoBytes[i++] & 0xFF) << 8 | (infoBytes[i++] & 0xFF);
+        byte[] entriesBytes = Arrays.copyOfRange(infoBytes, i, infoBytes.length);
+        this.entries = getEntries(numberOfEntries, new ByteArrayInputStream(entriesBytes));
     }
 
+    public StackMapFrame[] getEntries() {
+        return entries;
+    }
+
+    public void setEntries(StackMapFrame[] entries) {
+        this.entries = entries;
+    }
+
+    public static StackMapFrame[] getEntries(int numberOfEntries, InputStream in) throws Exception {
+        byte[] u1 = new byte[1];
+        byte[] u2 = new byte[2];
+        StackMapFrame[] stackMapFrames = new StackMapFrame[numberOfEntries];
+        for (int i = 0; i < numberOfEntries; i++) {
+            StackMapFrame stackMapFrame = new StackMapFrame();
+            in.read(u1);
+            int type = UToNumeric.u1ToInt(u1);
+            stackMapFrame.setFrameType(type);
+            if (type <= 63) {
+                // same_frame nothing need do
+            } else if (type <= 127) {
+                // same_locals_1_stack_item_frame
+                VerificationTypeInfo[] stacks = new VerificationTypeInfo[]{getVerificationTypeInfo(in)};
+                stackMapFrame.setStack(stacks);
+            } else if (type <= 247) {
+                // same_locals_1_stack_item_frame_extended
+                in.read(u2);
+                stackMapFrame.setOffsetDelta(UToNumeric.u2ToInt(u2));
+                VerificationTypeInfo[] stacks = new VerificationTypeInfo[]{getVerificationTypeInfo(in)};
+                stackMapFrame.setStack(stacks);
+            } else if (type <= 250) {
+                // chop_frame
+                in.read(u2);
+                stackMapFrame.setOffsetDelta(UToNumeric.u2ToInt(u2));
+            } else if (type == 251) {
+                // same_frame_extended
+                in.read(u2);
+                stackMapFrame.setOffsetDelta(UToNumeric.u2ToInt(u2));
+            } else if (type <= 254) {
+                // append_frame
+                in.read(u2);
+                stackMapFrame.setOffsetDelta(UToNumeric.u2ToInt(u2));
+                VerificationTypeInfo[] locals = new VerificationTypeInfo[type - 251];
+                for (int j = 0; j < type - 251; j++) {
+                    locals[j] = getVerificationTypeInfo(in);
+                }
+                stackMapFrame.setLocals(locals);
+            } else {
+                // full_frame
+                in.read(u2);
+                stackMapFrame.setOffsetDelta(UToNumeric.u2ToInt(u2));
+                in.read(u2);
+                int numOfLocals = UToNumeric.u2ToInt(u2);
+                VerificationTypeInfo[] locals = new VerificationTypeInfo[numOfLocals];
+                for (int j = 0; j < numOfLocals; j++) {
+                    locals[j] = getVerificationTypeInfo(in);
+                }
+                stackMapFrame.setLocals(locals);
+
+                in.read(u2);
+                int numOfStack = UToNumeric.u2ToInt(u2);
+                VerificationTypeInfo[] stacks = new VerificationTypeInfo[numOfStack];
+                for (int j = 0; j < numOfStack; j++) {
+                    stacks[j] = getVerificationTypeInfo(in);
+                }
+                stackMapFrame.setStack(stacks);
+            }
+            stackMapFrames[i] = stackMapFrame;
+        }
+
+        return stackMapFrames;
+    }
+
+    public static VerificationTypeInfo getVerificationTypeInfo(InputStream in) throws Exception {
+        VerificationTypeInfo verificationTypeInfo = new VerificationTypeInfo();
+        byte[] u1 = new byte[1];
+        byte[] u2 = new byte[2];
+        in.read(u1);
+        int tag = UToNumeric.u1ToInt(u1);
+        verificationTypeInfo.setTag(tag);
+        if (tag == 0) {
+            // ITEM_TOP
+        } else if (tag == 1) {
+            // ITEM_Integer
+        } else if (tag == 2) {
+            // ITEM_Float
+        } else if (tag == 4) {
+            // ITEM_Long
+        } else if (tag == 5) {
+            // ITEM_Double
+        } else if (tag == 6) {
+            // ITEM_UninitializedThis
+        } else if (tag == 7) {
+            // ITEM_Object
+            in.read(u2);
+            verificationTypeInfo.setCpoolIndex(UToNumeric.u2ToInt(u2));
+        } else if (tag == 8) {
+            // ITEM_Uninitialized
+            in.read(u2);
+            verificationTypeInfo.setOffset(UToNumeric.u2ToInt(u2));
+        }
+        return verificationTypeInfo;
+    }
+
+    public static class StackMapFrame {
+        private int frameType;
+        private int offsetDelta;
+        private VerificationTypeInfo[] locals;
+        private VerificationTypeInfo[] stack;
+
+        public int getFrameType() {
+            return frameType;
+        }
+
+        public void setFrameType(int frameType) {
+            this.frameType = frameType;
+        }
+
+        public int getOffsetDelta() {
+            return offsetDelta;
+        }
+
+        public void setOffsetDelta(int offsetDelta) {
+            this.offsetDelta = offsetDelta;
+        }
+
+        public VerificationTypeInfo[] getLocals() {
+            return locals;
+        }
+
+        public void setLocals(VerificationTypeInfo[] locals) {
+            this.locals = locals;
+        }
+
+        public VerificationTypeInfo[] getStack() {
+            return stack;
+        }
+
+        public void setStack(VerificationTypeInfo[] stack) {
+            this.stack = stack;
+        }
+    }
+
+    static class VerificationTypeInfo {
+        private int tag;
+        private int cpoolIndex;
+        private int offset;
+
+        public int getTag() {
+            return tag;
+        }
+
+        public void setTag(int tag) {
+            this.tag = tag;
+        }
+
+        public int getCpoolIndex() {
+            return cpoolIndex;
+        }
+
+        public void setCpoolIndex(int cpoolIndex) {
+            this.cpoolIndex = cpoolIndex;
+        }
+
+        public int getOffset() {
+            return offset;
+        }
+
+        public void setOffset(int offset) {
+            this.offset = offset;
+        }
+
+        //        union verification_type_info {
+//            Top_variable_info;
+//            Integer_variable_info;
+//            Float_variable_info;
+//            Long_variable_info;
+//            Double_variable_info;
+//            Null_variable_info;
+//            UninitializedThis_variable_info;
+//            Object_variable_info;
+//            Uninitialized_variable_info;
+//        }
+    }
 }
--- src/com/utils/UToNumeric.java
+++ src/com/utils/UToNumeric.java
@@ -6,37 +6,37 @@ import java.nio.ByteBuffer;
  * Created by chenjiaxu on 2017/10/23.
  */
 public class UToNumeric {
-    public static int u4ToInt(byte[] b) {
-        int temp = (b[3] & 0xFF) | (b[2] & 0xFF) << 8 | (b[1] & 0xFF) << 16 | (b[0] & 0xFF) << 24;
+    public static int u4ToInt(byte[] u4) {
+        int temp = (u4[3] & 0xFF) | (u4[2] & 0xFF) << 8 | (u4[1] & 0xFF) << 16 | (u4[0] & 0xFF) << 24;
         return temp;
     }
 
-    public static float u4ToFloat(byte[] b) {
-        int temp = u4ToInt(b);
+    public static float u4ToFloat(byte[] u4) {
+        int temp = u4ToInt(u4);
         return Float.intBitsToFloat(temp);
     }
 
-    public static long u8ToLong(byte[] b) {
+    public static long u8ToLong(byte[] u8) {
         ByteBuffer byteBuffer = ByteBuffer.allocate(8);
-        byteBuffer.put(b);
+        byteBuffer.put(u8);
         byteBuffer.flip();
         return byteBuffer.getLong();
     }
 
-    public static double u8ToDouble(byte[] b){
+    public static double u8ToDouble(byte[] u8) {
         ByteBuffer byteBuffer = ByteBuffer.allocate(8);
-        byteBuffer.put(b);
+        byteBuffer.put(u8);
         byteBuffer.flip();
         return byteBuffer.getDouble();
     }
 
-    public static int u2ToInt(byte[] b) {
-        int temp = (b[1] & 0xFF) | (b[0] & 0xFF) << 8;
+    public static int u2ToInt(byte[] u2) {
+        int temp = (u2[1] & 0xFF) | (u2[0] & 0xFF) << 8;
         return temp;
     }
 
-    public static int u1ToInt(byte[] b) {
-        int temp = b[0] & 0xFF;
+    public static int u1ToInt(byte[] u1) {
+        int temp = u1[0] & 0xFF;
         return temp;
     }
 }
